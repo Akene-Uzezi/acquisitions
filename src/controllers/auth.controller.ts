addc0e81168a5c4346bdb61c458e77f61617f99c
Akene Uzezi <xtremejoexj@gmail.com>
2026-05-08 13:26:09 +0100

    feat: add auth service, controllers, and utilities

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,7 @@
 import logger from '#config/logger.js';
+import { createUser } from '#services/auth.service.js';
+import { cookies } from '#utils/cookies.js';
+import { jwttoken } from '#utils/jwt.js';
 import { registerSchema } from '#validations/auth.validation.js';
 import { formatValidationError } from '#validations/format.js';
 import { Response, Request, NextFunction } from 'express';
@@ -15,17 +18,29 @@ export const register = async (
         details: formatValidationError(validationResult.error),
       });
     }
-    const { name, email, role } = validationResult.data;
+    const { name, email, password, role } = validationResult.data;
     //AUTH SERVICE
+    const userData = { name, email, password, role };
+    const user = await createUser(userData);
+
+    const token = await jwttoken.sign({
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    });
+
+    if (!token) throw new Error('Error signing the token');
+
+    cookies.set(res, 'token', token);
 
     logger.info('User registration successful');
     res.status(201).json({
       message: 'User Registered',
       user: {
-        id: 1,
-        name,
-        email,
-        role,
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
       },
     });
   } catch (e: unknown) {
---
+++ src/services/auth.service.ts
@@ -0,0 +1,52 @@
+import logger from '#config/logger.js';
+import bcrypt from 'bcrypt';
+import { CreateUser } from '#types/service.types.js';
+import { db } from '#config/database.js';
+import { users } from '#models/user.model.js';
+import { eq } from 'drizzle-orm';
+
+export const hashPassword = async (
+  password: string
+): Promise<string | undefined> => {
+  try {
+    return await bcrypt.hash(password, 10);
+  } catch (e) {
+    logger.error(`Error hashing the password ${e}`);
+    throw new Error(`Error hashing the password ${e}`);
+  }
+};
+
+export const createUser = async (userData: CreateUser) => {
+  try {
+    const { name, email, password, role } = userData;
+    const existingUser = db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
+    if ((await existingUser).length > 0) throw new Error('User already exists');
+    const passwordHash = await hashPassword(password);
+    if (!passwordHash) throw new Error('Error hashing the password');
+    const [newUser] = await db
+      .insert(users)
+      .values({
+        name,
+        email,
+        password: passwordHash,
+        role,
+      })
+      .returning({
+        id: users.id,
+        name: users.name,
+        email: users.email,
+        role: users.role,
+        createdAt: users.createdAt,
+      });
+
+    return newUser;
+    logger.info(`User ${newUser.email} created successfully`);
+  } catch (e) {
+    logger.error(`Error creating user ${e}`);
+    throw e;
+  }
+};
